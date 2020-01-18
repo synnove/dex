3868fe79703e27bc6311f15ea8dda04fe6559f51
synnove <crys.chua@gmail.com>
2020-01-19 09:29:46 +1000

    css updates, ui update, still working through this entire mess

--- src/App.js
+++ src/App.js
@@ -8,21 +8,21 @@ class PokeCheckBox extends React.Component {
     var checked = this.props.value;
     var type    = this.props.type;
 
-    if (this.props.value) {
-      checkbox = <input type="checkbox" disabled="disabled" checked="checked"/>;
+    if (checked) {
+      checkbox = <input type="checkbox" className="nes-checkbox" disabled="disabled" checked="checked"/>;
     } else {
-      checkbox = <input type="checkbox" disabled="disabled"/>;
+      checkbox = <input type="checkbox" className="nes-checkbox" disabled="disabled"/>;
     }
     
     return (
-      <div className="poke_info">{ type }: { checkbox }</div>
+      <div className="col-xs poke_info"><label>{ checkbox }<span>{ type }</span></label></div>
     )
   }
 }
 
 class PokeID extends React.Component {
   render () {
-    return <div className="pokemon_id">{ this.props.id }</div>
+    return <div className="col-xs pokemon_id">{ this.props.id }</div>
   }
 }
 
@@ -34,19 +34,19 @@ class PokeIcon extends React.Component {
     if (name) {
       if (type === "pokemon") {
         return (
-          <div className="poke_icon">
+          <div className="col-xs poke_icon">
               <img src={process.env.PUBLIC_URL + '/icons/' + name + '.png'} alt={ name }/>
           </div>
         )
       } else if (type === "pokeball") {
         return (
-          <div className="poke_ball">
+          <div className="col-xs poke_ball">
             <img src={process.env.PUBLIC_URL + '/icons/pokeballs/' + name.toLowerCase() + 'ball.png'} alt={ name }/>
           </div>
         )
       } else if (type === "gender") {
         return (
-          <div className="poke_ball">
+          <div className="col-xs poke_ball">
             <img src={process.env.PUBLIC_URL + '/icons/' + name.toLowerCase() + '.png'} alt={ name }/>
           </div>
         )
@@ -61,7 +61,7 @@ class PokeTypes extends React.Component {
   render () {
     var types = Array.from(this.props.types);
     return (
-      <div className="poke_types">
+      <div className="col-xs-2 poke_types">
         { types.map((type) => (
           <img src={process.env.PUBLIC_URL + '/icons/types/' + type.toLowerCase() + '.gif'} alt={ type }/>
         ))}
@@ -73,6 +73,7 @@ class PokeTypes extends React.Component {
 class PokeSelect extends React.Component {
   render () {
     var data     = this.props.data;
+    {/*
     var select   = this.props.select;
     var natures  = ["Adamant", "Bashful", "Bold", "Brave", "Calm", "Careful", "Docile", "Gentle", "Hardy", "Hasty", "Impish", "Jolly", "Lax", 
                     "Lonely", "Mild", "Modest", "Naive", "Naughty", "Quiet", "Quirky", "Rash", "Relaxed", "Sassy", "Serious", "Timid", "None"]
@@ -96,23 +97,24 @@ class PokeSelect extends React.Component {
         elements.push(<option key={ option }>{ option }</option>) 
       }
     });
-
+   
     return ( 
       <select disabled="disabled">
         { elements }
       </select> 
     )
+    */}
+    return (
+      <div className="col-xs poke_info">{ data }</div>
+    )
   }
-}
-
-class PokeBall extends React.Component {
 }
 
 class PokeGeneric extends React.Component {
   render () {
     var data = this.props.data;
     return (
-      <div className="poke_info"> { data } </div>
+      <div className="col-xs poke_info"> { data } </div>
     )
   }
 }
@@ -132,19 +134,22 @@ class Pokemon extends React.Component {
     var caught    = pokeData[ padded_id ]["Caught"];
     var owned     = pokeData[ padded_id ]["Owned"];
     return (
-      <div className="pokemon">
+      <div className="row nes-container is-rounded pokemon">
         <PokeID       id={ padded_id }                                  />
         <PokeIcon     name={ id }     type="pokemon"                    />
         <PokeGeneric  data={ name }                                     />
         <PokeTypes    types={ types }                                   />
         <PokeCheckBox type="Caught"   value={ caught }                  />
         <PokeCheckBox type="Owned"    value={ owned }                   />
+        <div className="col-xs"><button type="button" className="nes-btn is-disabled">View Stats</button></div>
+        {/*
         <PokeIcon     name={ ball }   type="pokeball"                   />
         <PokeSelect   select="gender" data={ gender }                   />
         <PokeSelect   select="nature" data={ nature }                   />
         <PokeGeneric  data={ "IVs: " + iv + "/6" }                      />
         <PokeGeneric  data={ "Dynamax: " + dynamax + "/10" }      />
         <PokeSelect   select="region" data={ language }                 />
+        */}
       </div>
     );
   }
@@ -162,8 +167,41 @@ class Region extends React.Component {
     }
 
     return (
-      <div className="box">
+      <div className="col-xs-12">
         { elements }
+      </div>
+    )
+  }
+}
+
+class Trainer extends React.Component {
+  render () {
+    var name = "Reine";
+    return (
+      <div id="trainer" className="container">
+        <div className="row nes-container with-title is-rounded">
+          <p className="title">{ name }</p>
+          <div className="col-xs-12"><div className="row">
+            <div className="col-xs-2 trainer">
+              <img src={process.env.PUBLIC_URL + '/icons/' + name + '.png'} alt={ name }/>
+            </div>
+            <div className="col-xs-10">
+              <div className="row">
+                <div className="col-xs nes-container is-rounded">nothing actually works yet</div>
+                <div className="col-xs nes-container is-rounded">badges: </div>
+                <div className="col-xs nes-container is-rounded">pokemon caught: 400</div>
+              </div>
+              <div className="row">
+                <div className="col-xs nes-container is-rounded pokemon"><img src={process.env.PUBLIC_URL + '/icons/9.png'} alt="Inteleon"/></div>
+                <div className="col-xs nes-container is-rounded pokemon"><img src={process.env.PUBLIC_URL + '/icons/69.png'} alt="Ninetales"/></div>
+                <div className="col-xs nes-container is-rounded pokemon"><img src={process.env.PUBLIC_URL + '/icons/209.png'} alt="Meowstic"/></div>
+                <div className="col-xs nes-container is-rounded pokemon"><img src={process.env.PUBLIC_URL + '/icons/219.png'} alt="Sirfetch'd"/></div>
+                <div className="col-xs nes-container is-rounded pokemon"><img src={process.env.PUBLIC_URL + '/icons/81.png'} alt="Froslass"/></div>
+                <div className="col-xs nes-container is-rounded pokemon"><img src={process.env.PUBLIC_URL + '/icons/311.png'} alt="Toxtricity"/></div>
+              </div>
+            </div>
+          </div></div>
+        </div>
       </div>
     )
   }
@@ -174,13 +212,17 @@ class Pokedex extends React.Component {
   render() {
     return (
       <div className="page">
-        <div className="header">
-          <button className="region">Galar Basic</button>
-          <button className="region">Isle of Whatever</button>
-          <button className="region">Something Tundra</button>
+        <div className="row center-xs header">
+          <button className="nes-btn is-primary">Galar (1-400)</button>
+          <button className="nes-btn is-disabled">Isle of Armor (???)</button>
+          <button className="nes-btn is-disabled">Crown Tundra (???)</button>
         </div>
-        <div className="pokedex">
-          <Region region="galar"/>
+        <Trainer/>
+        <div id="pokedex" className="container">
+          <div className="row nes-container with-title is-rounded">
+            <p className="title">Pokedex</p>
+            <Region region="galar"/>
+          </div>
         </div>
       </div>
     )
